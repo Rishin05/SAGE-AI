7010e8932c4913305d931bf4f3ca73c9b7ed2dce
Rishin05 <rishinp26@gmail.com>
2024-11-08 00:25:08 -0500

    vite changes to run on deployment site

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 5173, // Use 5173 as a fallback for local development
+    host: true, // Needed for Render to detect the correct port
+  },
   plugins: [react()],
 })
