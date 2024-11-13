c3f50587ec76d4008b52991e86394e17fc8d1ec4
Rishin05 <rishinp26@gmail.com>
2024-11-12 23:33:59 -0500

    main changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,44 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Dashboard from './pages/Dashboard/Dashboard.jsx'
-import Home from './pages/Homepage/Home.jsx'
-import Chatpage from './pages/chatPage/Chatpage.jsx'
-import RootLayout from './layouts/rootLayout/RootLayout.jsx'
-import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx'
-import Login from './pages/LogInPage/Login.jsx'
-import SignUpPage from './pages/signUpPage/SignUp.jsx'
-import PrivacyPage from './pages/privacyPage/PrivacyPage.jsx'
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react';
+import Dashboard from './pages/Dashboard/Dashboard.jsx';
+import Home from './pages/Homepage/Home.jsx';
+import Chatpage from './pages/chatPage/Chatpage.jsx';
+import RootLayout from './layouts/rootLayout/RootLayout.jsx';
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout.jsx';
+import Login from './pages/LogInPage/Login.jsx';
+import SignUpPage from './pages/signUpPage/SignUp.jsx';
+import PrivacyPage from './pages/privacyPage/PrivacyPage.jsx';
 
 const router = createBrowserRouter([
   {
-    element: <RootLayout/>,
+    element: <RootLayout />,
     children: [
+      { path: "/", element: <Home /> },
+      { path: "/privacy", element: <PrivacyPage /> },
+      { path: "/log-in/*", element: <Login /> },
+      { path: "/sign-up/*", element: <SignUpPage /> },
       {
-        path: "/", element: <Home/>
-      },
-      {
-        path: "/privacy", element: <PrivacyPage/>
-      },
-      {
-        path: "/log-in/*", element: <Login/>
-      },
-      {
-        path: "/sign-up/*", element: <SignUpPage/>
-      },
-      {
-        element: <DashboardLayout/>,
-        children:[
-          {
-            path: "/dashboard", element:<Dashboard/>
-          },
-          {
-            path: "/dashboard/chats/:id", element:<Chatpage/>
-          },
+        element: <DashboardLayout />,
+        children: [
+          { path: "/dashboard", element: <Dashboard /> },
+          { path: "/dashboard/chats/:id", element: <Chatpage /> },
         ],
       },
     ],
@@ -47,6 +33,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ClerkProvider frontendApi={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </React.StrictMode>
+);
